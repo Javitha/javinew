cf0e774e1ba22062da2bfde23af5b215f12eff9c
Javitha <javithajayaraman@gmail.com>
2016-07-10 12:19:47 +0530

    Create evenodd.cpp

---
+++ evenodd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+int main()
+{
+    long n;
+    cin>>n;
+    if(n%2==0)
+    cout<<"EVEN NUMBER";
+    else
+    cout<<"ODD NUMBER";
+    return 0;
+}
