ab603659ccca14341c1ce11833ca8c40e7a25498
Javitha <javithajayaraman@gmail.com>
2016-07-10 17:19:07 +0530

    Create camelcase.cpp

---
+++ camelcase.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cmath>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+    string str;
+    getline(cin,str);
+    str[0]=toupper(str[0]);
+        for(int j=0;j<str.length();j++)
+    {
+        if(str[j]==' ')
+            str[j+1]=toupper(str[j+1]);
+        
+    }
+     for(int j=0;j<str.length();j++)
+    {
+       
+            cout<<str[j];
+        
+    }
+    
+    
+    
+ return 0;   
+}
